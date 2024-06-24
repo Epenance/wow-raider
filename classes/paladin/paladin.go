c8bd1543896c476e83b4b2e8103486a20abc12e5
Martin Hobert <info@dosh.dk>
2024-06-24 13:54:49 +0200

    add pala and ret state

--- classes/paladin/paladin.go
+++ classes/paladin/paladin.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"time"
 	"wow-raider/classes"
+	"wow-raider/util"
 )
 
 type PaladinState struct {
@@ -46,4 +47,55 @@ func (c *Paladin) SetState() {
 
 	// Hack because there is no inheritance in Go
 	c.SyncState(&c.BaseClass.State, &c.State)
+
+	// Paladin specific state
+	c.State.CrusaderStrikeAvailable = c.CheckColor(util.BLUE, 25, 0)
+	c.State.IsJudgementReady = c.CheckColor(util.BLUE, 5, 0)
+	c.State.ShouldAoE = c.CheckColor(util.GREEN, 35, 0)
+	c.State.ConsecrationAvailable = c.CheckColor(util.YELLOW, 45, 0)
+	c.State.BlessingOn = !c.CheckColor(util.RED, 25, 5)
+	c.State.HammerOfWrathAvailable = c.CheckColor(util.BLUE, 20, 0)
+	c.State.AvengeWrathAvailable = c.CheckColor(util.BLUE, 65, 5)
+	c.State.AvengingWrathActive = c.CheckColor(util.GREEN, 60, 5)
+}
+
+func (c *Paladin) SetZeal() {
+	noActiveSeal := c.CheckColor(util.RED, 0, 0)
+
+	if noActiveSeal {
+		c.State.ActiveSeal = "none"
+		return
+	}
+
+	activeSealIsRighteousness := c.CheckColor(util.GREEN, 50, 0)
+
+	if activeSealIsRighteousness {
+		c.State.ActiveSeal = "Seal of Righteousness"
+		return
+	}
+
+	activeSealIsTruth := c.CheckColor(util.BLUE, 50, 0)
+
+	if activeSealIsTruth {
+		c.State.ActiveSeal = "Seal of Truth"
+		return
+	}
+}
+
+func (c *Paladin) SetHolyPower() {
+	isHolyPower1 := c.CheckColor(util.GREEN, 45, 5)
+	isHolyPower2 := c.CheckColor(util.BLUE, 45, 5)
+	isHolyPower3 := c.CheckColor(util.YELLOW, 45, 5)
+
+	if isHolyPower1 {
+		c.State.HolyPower = 1
+	}
+
+	if isHolyPower2 {
+		c.State.HolyPower = 2
+	}
+
+	if isHolyPower3 {
+		c.State.HolyPower = 3
+	}
 }
--- classes/paladin/retribution.go
+++ classes/paladin/retribution.go
@@ -2,10 +2,18 @@ package paladin
 
 import (
 	"fmt"
+	"wow-raider/util"
 )
 
 type RetributionState struct {
 	PaladinState
+	DivineStormAvailable bool
+	ArtOfWarActive       bool
+	TemplarsVerdictReady bool
+	DivinePurposeActive  bool
+	InquisitionActive    bool
+	ZealotryActive       bool
+	ZealotryAvailable    bool
 }
 
 type Retribution struct {
@@ -45,6 +53,14 @@ func (c *Retribution) SetState() {
 
 	// Hack because there is no inheritance in Go
 	c.SyncState(&c.Paladin.State, &c.State)
+
+	c.State.DivineStormAvailable = c.CheckColor(util.BLUE, 30, 0)
+	c.State.ArtOfWarActive = c.CheckColor(util.BLUE, 20, 5)
+	c.State.TemplarsVerdictReady = c.CheckColor(util.BLUE, 50, 5)
+	c.State.DivinePurposeActive = c.CheckColor(util.BLUE, 55, 5)
+	c.State.InquisitionActive = c.CheckColor(util.GREEN, 55, 0)
+	c.State.ZealotryActive = c.CheckColor(util.GREEN, 60, 0)
+	c.State.ZealotryAvailable = c.CheckColor(util.BLUE, 65, 0)
 }
 
 func (c *Retribution) Rotation() {
